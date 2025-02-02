eff26eb0a30789ce89888995151e59a52c75f908
Aleksandr Petrov <petrovalek2@gmail.com>
2025-02-02 23:21:24 +0300

    asd

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -3,5 +3,8 @@ export default defineEventHandler((event) => {
     
     // event.context.path to get the route path: '/api/foo/bar/baz'
     // event.context.params._ to get the route segment: 'bar/baz'
+
+    event.setHeader('Content-Type', 'application/json');
+    event.setHeader('Access-Control-Allow-Origin', '*');
     return event.node.req.rawHeaders
   })

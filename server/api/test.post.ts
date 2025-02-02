b99fff8d1ddab00d10d231a72458e9073f0629de
Aleksandr Petrov <petrovalek2@gmail.com>
2025-02-03 01:12:20 +0300

    123

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,19 +1,12 @@
 export default defineEventHandler((event) => {
-    console.log(event.node.req.headers, 'event.node.req.headers');
-
-    // event.context.path to get the route path: '/api/foo/bar/baz'
-    // event.context.params._ to get the route segment: 'bar/baz'
-    // response.setHeader('Content-Type', 'application/json');
-    // response.setHeader('Access-Control-Allow-Origin', '*');
-    // event.setHeader('Content-Type', 'application/json');
-    // event.node.req.setHeader('Access-Control-Allow-Origin', '*');
-
     setResponseHeaders(event, {
         "Content-Type": 'application/json',
         'Access-Control-Allow-Origin': '*'
     });
 
-    console.log(event,'event -------------------');
+    // console.log(event, 'event -------------------');
 
-    return event.node.req.headers
+    // console.log(event.node.req.originalUrl, 'event.originalUrl -------------------');
+
+    return { headers: event.node.req.headers, originalUrl: event.node.req.originalUrl }
   })
